edc7cf3b3338b4ba30bb375e55028e0794fd96a6
alonzomaco <stk_neji@hotmail.com>
2015-11-03 16:01:13 -0500

    Modificando Proyecto

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Sistema de Ventas</title>
+        <title>Sistema de Ventas por Alonzo</title>
     </head>
     <body>
         <?php
